52759dbb7e1379ffdf16d4e6808324d938c84b20
TOUFIQUER RAHMAN <toufiquerabc@gmail.com>
2023-06-10 07:56:32 +0600

    add jwt success

--- index.js
+++ index.js
@@ -6,12 +6,28 @@ const port = process.env.PORT || 5000;
 const jwt = require("jsonwebtoken");
 const data = require("./data.json");
 const review = require("./review.json");
-// json web token
 
+// json web token
 app.use(express.json());
 app.use(cors());
 
+// Add MongoDB connection
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.mxvrtkz.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+client.connect();
 async function run() {
+  // create a database
+  const usersCollection = client.db("usersCollection").collection("users");
   console.log("Node is running", " => Line No: 29");
 
   app.get("/", (req, res) => {
@@ -22,13 +38,21 @@ async function run() {
   });
 
   // Create an access token
-  app.post("/jwt", (req, res) => {
+  app.post("/jwt", async (req, res) => {
     const email = req.body.email;
     const data = { email };
     const token = jwt.sign(data, process.env.TOKEN_SECRET, {
       expiresIn: "1h",
     });
-    res.send({ token });
+
+    // save to database email and role
+    const user = {
+      email,
+      role: "student",
+    };
+    const result = await usersCollection.insertOne(user);
+    result.token = token;
+    res.send({ result });
   });
 }
 run().catch(console.dir);
